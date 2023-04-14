a7f9283cbcf3cf2f4b48e33d85b22aa99ce2da47
GojoSatoruSakthi <130234971+GojoSatoruSakthi@users.noreply.github.com>
2023-04-14 23:41:29 +0530

    Update App.py

--- Smart_Resume_Analyser_App/App.py
+++ Smart_Resume_Analyser_App/App.py
@@ -18,7 +18,8 @@ from pdfminer3.converter import TextConverter
 import io,random
 import spacy
 import nltk
-nltk.download('stopwords')
+from nltk.corpus import stopwords
+
 from streamlit_tags import st_tags
 from PIL import Image
 import pymysql
